c806e0ee1782c756e1a2f097d49695e7f4ddf50b
zOzeca <ze.lupicaetano@gmail.com>
2018-11-16 18:25:51 +0000

    home with animating date number

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
 import { Container, Row, Col, Card, Table } from 'react-bootstrap'
+import CountUp from 'react-countup';
+import moment from 'moment'
+import 'moment-timezone';
 
 class Home extends Component {
   render() {
+    let lastSupression = moment("20111031").fromNow().trim()
+    let lastSupressionValue = parseInt(lastSupression)
+    let lastSupressionExpression = lastSupression.match(/[^\d]*/g)
+    let lastSupressionLabel = lastSupressionExpression.filter(word => word !== '')
+
     return (
       <div className="Home">
         <Container>
@@ -10,7 +18,15 @@ class Home extends Component {
             <Col xs={12}>
               <Card>
                 <Card.Body className="text-center">
-                  <h1>X Suprimidos em Y tempo</h1>
+                  <h1>O último comboio suprimido foi à
+                    <CountUp
+                      start={0}
+                      end={lastSupressionValue}
+                      duration={3}
+                      delay={0.5}
+                    />
+                    {lastSupressionLabel}
+                  </h1>
                   <Table responsive>
                     <thead>
                       <tr>
