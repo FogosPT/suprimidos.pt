6c856f0861bbae80a650300b8976ec4ce57625b2
Ricardo Pimentel <ricardojrgpimentel@gmail.com>
2018-11-21 10:43:27 +0000

    added proper time for delays

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,7 +20,7 @@ class Home extends Component {
     }
   }
 
-  handleTimeToAnimate(date) {
+  handleTimeToAnimate() {
     let lastSupression
     let numberValue = false
     let prefixString = 'há muito'
@@ -87,24 +87,24 @@ class Home extends Component {
       <tr key={location.key}>
         <td>{location.value}</td>
         {content.map((item, index) => (
-            <td key={index}>{this.renderCount(item.count)}</td>
+          <td key={index}>{this.renderCount(item.count)}</td>
         ))}
       </tr>
     )
   }
 
   renderCount(count) {
-    if(count){
+    if (count) {
       let randomStart = Math.floor(Math.random() * (20 - 1 + 1)) + 1;
 
-      return  <CountUp
-          start={randomStart}
-          end={parseInt(count,10)}
-          duration={4}
-          delay={1}
-        />
+      return <CountUp
+        start={randomStart}
+        end={parseInt(count, 10)}
+        duration={4}
+        delay={1}
+      />
     } else {
-        return <i class="fas fa-check-circle"></i>
+      return <i className="fas fa-check-circle"></i>
     }
   }
 
