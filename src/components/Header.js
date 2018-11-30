5a8957c96c48f06ac4df30b70efe33dc18d0193f
tomahock <eu@tomahock.com>
2018-11-30 13:45:00 +0000

    add notifications, android and ios

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-import { Container, Navbar, Nav } from 'react-bootstrap'
+import { Container, Navbar, Nav, OverlayTrigger,Tooltip } from 'react-bootstrap'
 
 class Header extends React.Component {
   render() {
@@ -13,8 +13,10 @@ class Header extends React.Component {
             <Nav>
               <Nav><Link className='nav-link' to="/">Home</Link></Nav>
               <Nav><Link className='nav-link' to="/atrasos">Atrasos</Link></Nav>
-              {/* <Nav><Link className='nav-link' to="/notificacoes">Notificações</Link></Nav> */}
+              <Nav><Link className='nav-link' to="/notificacoes">Notificações</Link></Nav>
               <Nav><a className='nav-link' href="https://twitter.com/suprimidospt" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a></Nav>
+              <Nav><a className='nav-link' href="https://play.google.com/apps/testing/com.tomahock.suprimidos" target="_blank" rel="noopener noreferrer"><i className="fab fa-android"></i></a></Nav>
+              <Nav><LinkWithTooltip tooltip={<span>Brevemente</span>} href="#" id="tooltip"><i className="fab fa-apple"></i></LinkWithTooltip></Nav>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -22,5 +24,19 @@ class Header extends React.Component {
     )
   }
 }
+
+function LinkWithTooltip({ id, children, href, tooltip }) {
+  return (
+    <OverlayTrigger
+      overlay={<Tooltip id={id}>{tooltip}</Tooltip>}
+      placement="bottom"
+      delayShow={300}
+      delayHide={150}
+    >
+      <a className='nav-link' href={href}>{children}</a>
+    </OverlayTrigger>
+  );
+}
+
 
 export default Header
